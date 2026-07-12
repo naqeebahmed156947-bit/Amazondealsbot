d8839dca2f9606bf75f93ae43c9b8b87ed5e9397
naqeebahmed156947-bit <naqeebahmed156947@gmail.com>
2026-07-12 03:06:53 +0300

    Add Telegram bot integration to send messages

--- Main.py
+++ Main.py
@@ -1,1 +1,22 @@
 
+import json
+import os
+import requests
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHANNEL = os.getenv("TELEGRAM_CHANNEL")
+
+with open("posts.json", "r", encoding="utf-8") as f:
+    posts = json.load(f)
+
+for post in posts:
+    message = post["text"]
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHANNEL,
+        "text": message
+    }
+
+    requests.post(url, data=data)
